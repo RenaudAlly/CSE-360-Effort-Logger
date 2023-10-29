0ed63c94a804f8de8721f754d159a2a86045047d
ally.c <77428502+RenaudAlly@users.noreply.github.com>
2023-10-29 16:09:45 -0700

    Rebase
    Finished adding code from the latest tutorials with login page implemented and directing to a display page for username, password and password length. Pull and branch from here to work on your pages and scenes.

---
+++ src/application/LoginViewController.java
@@ -0,0 +1,37 @@
+package application;
+
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+
+public class LoginViewController {
+	private Scene scene;
+	private Stage stage;
+	private Login login = new Login();
+
+	@FXML
+	private TextField usernameField;
+	@FXML
+	private PasswordField passwordField;
+
+	public void submitLogin(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DisplayPage.fxml"));
+		login.setName(usernameField.getText());
+		login.setPassword(passwordField.getText());
+
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(fxmlLoader.load(), 900, 600);
+		stage.setTitle("Login Info");
+
+		DisplayViewController control = fxmlLoader.getController();
+		control.setLogin(login);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
