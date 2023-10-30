ccbccaa948b94e0d80f813d28bd2b70c9fc641d7
Matt Wang <77259515+matthewwangg@users.noreply.github.com>
2023-10-29 20:19:39 -0700

    Add files via upload

---
+++ src/application/ID.java
@@ -0,0 +1,39 @@
+package application;
+
+public class ID {
+
+	private String user;
+	private String pass;
+	
+	public ID() {
+		
+		user = "";
+		pass = "";
+		
+	}
+	
+	public String getUser() {
+		
+		return user;
+		
+	}
+	
+	public String getPass() {
+		
+		return pass;
+		
+	}
+	
+	public void setUser(String s) {
+		
+		this.user = s;
+		
+	}
+	
+	public void setPass(String s) {
+		
+		this.pass = s;
+		
+	}
+	
+}
