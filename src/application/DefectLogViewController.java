d3cc1a44a3a9038a787e0d4afbe69b9c5fc915ae
Matt Wang <77259515+matthewwangg@users.noreply.github.com>
2023-11-12 17:04:23 -0700

    Phase 5 Matt's Part
    Login, SignUp, Defect Console, Defect Log, some buttons need to be connected to Sawyer's and Adam's prototype

---
+++ src/application/DefectLogViewController.java
@@ -0,0 +1,68 @@
+package application;
+
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DefectLogViewController {
+	private Stage stage;
+	private Scene scene;
+	
+	@FXML
+	private Button ReturnButton;
+	
+	private ArrayList<Defect> array;
+	
+	@FXML
+	private TableColumn<Defect, String> DefectName, DefectCategory, InjectStep, RemoveStep, Status, DefectSymptoms, Project;
+	@FXML
+	private TableView<Defect> DefectLog;
+	
+	public void setArray(ArrayList<Defect> array) {
+		
+		this.array = array;
+		
+		if (this.array.isEmpty()) {
+			
+			return;
+			
+		}
+		else {
+		
+			DefectName.setCellValueFactory(new PropertyValueFactory<Defect, String>("name"));
+			DefectCategory.setCellValueFactory(new PropertyValueFactory<Defect, String>("category"));
+			InjectStep.setCellValueFactory(new PropertyValueFactory<Defect, String>("inject"));
+			RemoveStep.setCellValueFactory(new PropertyValueFactory<Defect, String>("remove"));
+			Status.setCellValueFactory(new PropertyValueFactory<Defect, String>("status"));
+			DefectSymptoms.setCellValueFactory(new PropertyValueFactory<Defect, String>("symptoms"));
+			Project.setCellValueFactory(new PropertyValueFactory<Defect, String>("project"));
+			ObservableList<Defect> list = FXCollections.observableArrayList(this.array);
+			DefectLog.setItems(list);
+			
+		}
+		
+	}
+	
+	public void ReturnOnAction(ActionEvent event) throws IOException {
+		
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DefectConsole.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(fxmlLoader.load(), 750, 500);
+		stage.setTitle("EffortLogger V2");
+		
+		DefectViewController control = fxmlLoader.getController();
+		control.setArray(array);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+}
---
+++ src/application/LoginViewController.java
@@ -0,0 +1,184 @@
+package application;
+
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+
+
+
+public class LoginViewController {
+	private Scene scene;
+	private Stage stage;
+	private Login login = new Login();
+
+	@FXML
+	private TextField UsernameInput;
+	@FXML
+	private PasswordField PasswordInput;
+	@FXML
+	private Label SignUpText;
+	@FXML
+	private ComboBox<String> ComboBoxInput; 
+	
+	public void SignUpButtonOnAction(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SignUpScreen.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(fxmlLoader.load(), 750, 500);
+		stage.setTitle("Sign Up Page");
+		
+		SignUpViewController control = fxmlLoader.getController();
+		stage.setScene(scene);
+		stage.show();
+		
+	
+	}
+	
+	public void LogInButtonOnAction(ActionEvent event) throws IOException {
+		
+		// first hard-coded credential
+		ID userid = new ID();
+		userid.setUser("adam");
+		userid.setPass("bonnet");
+		userid.setLevel(2);
+					
+		// second hard-coded credential
+		ID userid2 = new ID();
+		userid2.setUser("matt");
+		userid2.setPass("wang");
+		userid2.setLevel(2);
+					
+		// third hard-coded credential
+		ID userid3 = new ID();
+		userid3.setUser("arjun");
+		userid3.setPass("khetan");
+		userid3.setLevel(1);
+		
+		// third hard-coded credential
+		ID userid4 = new ID();
+		userid4.setUser("sawyer");
+		userid4.setPass("kesti");
+		userid4.setLevel(2);
+		
+		// third hard-coded credential
+		ID userid5 = new ID();
+		userid5.setUser("meshach");
+		userid5.setPass("samuel");
+		userid5.setLevel(0);
+		
+		ID useridfinal = new ID();
+				
+		// the array of credentials
+		ID[] arr = new ID[]{userid, userid2, userid3, userid4, userid5};
+		// save the username input and password input to string
+		String username = UsernameInput.getText().toString();
+		String password = PasswordInput.getText().toString();
+		
+		// if the username or password fall out of the range of acceptable number of characters
+		if (username.length() > 32 || password.length() > 32) {
+			
+			// display an invalid message that instructs them to change it
+			SignUpText.setText("Please enter an input that is 32 characters or less.");
+			
+		}
+		
+		// if something has been inputted in both the username and password fields
+		else if (username.length() > 0 && password.length() > 0) {
+			
+			// initialize the boolean that indicates whether the username password combo is valid
+			boolean found = false;
+			
+			boolean perm = false;
+			
+			// for all elements in the array
+			for (int i = 0; i < 5; i++) {
+				
+				// if the username password combo is in this element of the array
+				if (username.equals(arr[i].getUser()) && password.equals(arr[i].getPass())) {
+					
+					if (arr[i].getLevel() != 0) {
+						
+						// update found to be true
+						found = true;
+						useridfinal = arr[i];
+						break;
+						
+					}
+					else {
+						
+						SignUpText.setText("Your account does not have the permissions.");
+						perm = true;
+						break;
+						
+					}
+					
+				}
+				
+			}
+			
+			
+			
+			// if found is true, meaning the username and password are valid
+			if (found == true) {
+				
+				String go = ComboBoxInput.getValue().toString();
+				
+				if (go.equals("Effort Logger")) {
+					
+					FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DefectConsole.fxml"));
+					stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+					scene = new Scene(fxmlLoader.load(), 750, 500);
+					stage.setTitle("EffortLogger V2");
+					
+					DefectViewController control = fxmlLoader.getController();
+					control.setUser(useridfinal);
+					stage.setScene(scene);
+					stage.show();
+					
+				}
+				else {
+					
+					
+					
+				}
+				
+				
+			}
+			
+			// if the username or password is not valid
+			else {
+				
+				if (perm == false) {
+					
+					// display an invalid message that tells the user to try again
+					SignUpText.setText("Invalid Username or Password! Please try again. ");
+					
+				}
+				
+			}
+		
+		
+		}
+	
+	/*
+	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DisplayPage.fxml"));
+	login.setName(usernameField.getText());
+	login.setPassword(passwordField.getText());
+
+	stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	scene = new Scene(fxmlLoader.load(), 900, 600);
+	stage.setTitle("Login Info");
+
+	DisplayViewController control = fxmlLoader.getController();
+	control.setLogin(login);
+	stage.setScene(scene);
+	stage.show()
+	*/
+	}
+	
+}
